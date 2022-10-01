93d11ed8fe565f54a0d8b47287df5edd13cec419
Wade Graves <wadegraves940@gmail.com>
2022-10-01 15:15:45 -0400

    adding styling for each recipe displayed

--- src/component/Recipe.js
+++ src/component/Recipe.js
@@ -1,9 +1,16 @@
 const Recipe = ({title, calories, image}) => {
     return(
-        <div>
+        <div style={{
+            'display': 'inline-block',
+            'width': '32vw',
+            'height': '35vh',
+            'border': '1px solid black',
+            'margin' : '3px',
+            'position': 'relative'
+        }}>
             <h1>{title}</h1>
             <p>{calories}</p>
-            <img src = {image} alt = "" />
+            <img src = {image} alt = "" width="250" height="200" />
         </div>
     )
 }
