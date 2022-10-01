53e62c5ca8bff43a405f8f15b8bda684980ff51b
ivy <aibiimaknae@gmail.com>
2022-10-01 13:31:41 -0500

    basic login form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
+import LoginForm from './component/LoginForm';
 
 function App() {
   const logUser = {
@@ -19,7 +20,22 @@ function App() {
 
   return (
     <div className="App">
-      
+      {(user.email !="") ? (
+        <div className='welcome'>
+          <h2>welcome, <span>{user.name}</span></h2>
+          <button>Logout</button>
+          </div>
+          ) : (
+            <LoginForm />
+          )}
+    </div>
+  );
+}
+
+function App(){
+  return (
+    <div className = "App">
+
     </div>
   );
 }
---
+++ src/component/LoginForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Form } from 'react-router-dom';
+
+function LoginForm() {
+  return (
+    <form>
+        <div className='form-inner'>
+            <h2>Login</h2>
+            <div className='form-group'>
+                <label htmlFor='name'>Name:</label>
+                <input type='text' name='name' id='name'/>
+            </div>
+            <div className='form-group'>
+                <label htmlFor='email'>Email:</label>
+                <input type='email' name='email' id='email' />
+            </div>
+            <div className='form-group'>
+                <label htmlFor='password'>Password:</label>
+                <input type='password' name='password' id='password />'
+            </div>
+        </div>
+    </form>
+  )
+}
+
+export default LoginForm
