5bbf8d83cd8a6834ac5f5de74738feeb6ae66e0b
ivy <aibiimaknae@gmail.com>
2022-10-01 13:11:36 -0500

    started setting up functions for login and logout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const logUser = {
+    email: "",
+    password: "",
+  }
+
+  const [user, setUser] = useState({name: "", email: ""})
+
+  const Login = details => {
+    console.log(details);
+  }
+
+  const Logout = () => {
+    console.log("logout");
+  }
+
   return (
     <div className="App">
       
