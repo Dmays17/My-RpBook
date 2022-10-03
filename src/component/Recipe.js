22219662c3b25e5ce9646d66862e83503cc42dcf
Wade Graves <wadegraves940@gmail.com>
2022-10-03 19:42:56 -0400

    branch switch commit

--- src/component/Recipe.js
+++ src/component/Recipe.js
@@ -2,14 +2,14 @@ const Recipe = ({title, calories, image}) => {
     return(
         <div style={{
             'display': 'inline-block',
-            'width': '32vw',
+            'width': '35vw',
             'height': '35vh',
             'border': '1px solid black',
             'margin' : '3px',
             'position': 'relative'
         }}>
             <h1>{title}</h1>
-            <p>{calories}</p>
+            <p>Number of calories: {calories}</p>
             <img src = {image} alt = "" width="250" height="200" />
         </div>
     )
