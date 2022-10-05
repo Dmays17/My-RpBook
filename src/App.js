0a5517da8268dc380469a05d9404e69349d67d1c
ivy <aibiimaknae@gmail.com>
2022-10-05 18:43:41 -0500

    updated login

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import LoginForm from './component/LoginForm';
+import LoginForm from './components/LoginForm';
 
 function App() {
-  const logUser = {
-    email: "",
-    password: "",
+  const adminUser = {
+    name: "",
+    username: "admin",
+    password: "admin123"
   }
 
-  const [user, setUser] = useState({name: "", email: ""})
+  const [user, setUser] = useState({name: "", username: ""});
+  const [error, setError] = useState("");
 
   const Login = details => {
     console.log(details);
+
+    if (details.username == adminUser.username && details.password == adminUser.password){
+    console.log("Logged in");
+    setUser({
+      name: details.name,
+      username: details.username
+    });
+  } else {
+    console.log("Details do not match");
+    setError("Details do not match")
   }
+}  
 
   const Logout = () => {
-    console.log("logout");
+    setUser({ name:"", username: ""})
   }
 
   return (
     <div className="App">
-      {(user.email !="") ? (
-        <div className='welcome'>
-          <h2>welcome, <span>{user.name}</span></h2>
-          <button>Logout</button>
-          </div>
-          ) : (
-            <LoginForm />
-          )}
-    </div>
-  );
-}
-
-function App(){
-  return (
-    <div className = "App">
-
+     {(user.username != "") ? (
+      <div className="welcome">
+        <h2>Welcome, <span>{user.name}</span></h2>
+        <button onClick={Logout}>Logout</button>
+      </div>
+     ) : (
+      <LoginForm Login={Login} error={error} />
+     )} 
     </div>
   );
 }
--- src/component/LoginForm.js
+++ src/component/LoginForm.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import { Form } from 'react-router-dom';
+import React, {useState} from 'react';
 
-function LoginForm() {
+function LoginForm({ Login, error }) {
+  const [details, setDetails] = useState({name: "", username: "", password: ""});
+
+    const submitHandler = e => {
+      e.preventDefault();
+
+      Login(details);
+    }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
         <div className='form-inner'>
             <h2>Login</h2>
+            {(error != "") ? (<div className='error'>{error}</div>) : "" }
             <div className='form-group'>
                 <label htmlFor='name'>Name:</label>
-                <input type='text' name='name' id='name'/>
+                <input type='text' name='name' id='name' onChange={e => setDetails({...details, name: e.target.value})} value={details.name} />
             </div>
             <div className='form-group'>
-                <label htmlFor='email'>Email:</label>
-                <input type='email' name='email' id='email' />
+                <label htmlFor='username'>Username:</label>
+                <input type='username' name='username' id='username' onChange={e => setDetails({...details, username: e.target.value})} value={details.username} />
             </div>
             <div className='form-group'>
-                <label htmlFor='password'>Password:</label>
-                <input type='password' name='password' id='password />'
-            </div>
+                <label htmlFor='password'>Password</label>
+                <input type='password' name='password' id='password' onChange={e => setDetails({...details, password: e.target.value})} value={details.password} />
+              </div>
+                <input type="submit" value="Login" />
         </div>
     </form>
   )
