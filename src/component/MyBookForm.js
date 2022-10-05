a399cfc13e1ddc40a820a1a8e89ec1b35f188762
Dmays17 <101609429+Dmays17@users.noreply.github.com>
2022-10-05 15:23:33 -0500

    update

--- src/component/MyBookForm.js
+++ src/component/MyBookForm.js
@@ -24,27 +24,29 @@ function MyBookForm(){
       const handleClick = (e) => {
         e.preventDefault()  
         axios
-        .post("",Form)
+        .post("http://localhost:5002/create",Form)
          .then((res)=>console.log(res))
          .catch((err)=> console.log(err))
-      
+        
          
          
         };
 return(
+  <div>
     <form>
         <label form="name">name</label><br/>
-        <input type="name" name="name" placeholder="Name of recipe"></input><br/>
+        <input type="name" name="name" value={Form.name} placeholder="Name of recipe" onChange={handleChange}></input><br/>
         <label form="ingredents">ingredents</label><br/>
-        <input type="text" name="ingredents" placeholder="ingredents"></input><br/>
+        <input type="text" name="ingredents" value={Form.ingredents}placeholder="ingredents" onChange={handleChange}></input><br/>
         <label form="prep">prep</label><br/>
-        <input type="text" name="prep" placeholder="prep"></input><br/>
+        <input type="text" name="prep" value={Form.prep}placeholder="prep" onChange={handleChange}></input><br/>
         <label form="cook">cook</label><br/>
-        <input type="text" name="cook" placeholder="cooking instruction"></input><br/>
+        <input type="text" name="cook" value={Form.cook} placeholder="cooking instruction" onChange={handleChange}></input><br/>
             <button onClick={handleClick} variant="primary" type="submit">
             submit
             </button>
     </form>
+    </div>
 )
 }
 export default MyBookForm
